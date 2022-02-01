f5042c5736f3610900597ddb5d8e5f5674ab40de
Ramixzx <ramiro.mijalovich@gmail.com>
2022-02-01 10:40:27 -0300

    Se agregan breves comentarios al ejercicio 1. Se finaliza con el ejercicio 2

--- src/ejercicio1/index.js
+++ src/ejercicio1/index.js
@@ -1,11 +1,14 @@
+// Se le da un inicio y un limite de vueltas al bucle con estas variables.
 let vuelta = 1;
 let limite = 17;
 
+// Loop con while
 while (vuelta <= limite) {
     console.log(`Vuelta numero ${vuelta}`);
     vuelta++;
 };
 
+// Loop con for
 for(vuelta; vuelta <= limite; vuelta++){
     console.log(`Vueltita numero ${vuelta}`);
 };
---
+++ src/ejercicio2/index.js
@@ -0,0 +1,14 @@
+// Se le da un inicio y un final al bucle con estas variables.
+let inicio = 7;
+let final = 12;
+
+// Loop con for
+for(inicio; inicio <= final; inicio++){
+    console.log(`Vueltita numero ${inicio}`);
+};
+
+// Loop con while
+while (inicio <= final) {
+    console.log(`Vuelta numero ${inicio}`);
+    inicio++;
+};
