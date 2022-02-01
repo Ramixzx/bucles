6a6d1bd8507a95c653f7b689b9bb1b6756bd679e
Ramixzx <ramiro.mijalovich@gmail.com>
2022-02-01 13:19:37 -0300

    The exercise is over. For, for of, while and do while loops were used.

---
+++ src/ejercicio5/index.js
@@ -0,0 +1,28 @@
+var myArray =[232,32,1,4,55,4,3,32,3,24,5,5,5,34,2,3,5,5365743,52,34,3,55,33,435,4,6,54,63,45,4,67,56,47,1,34,54,32,54,1,78,98,0,9,8,98,76,7,54,2,3,42,456,4,3321,5];
+
+// Utilizando el ciclo for
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+};
+
+// Utilizando el ciclo for of
+for (let elemento of myArray){
+    console.log(elemento);
+};
+
+// Utilizando el ciclo while
+let index = 0;
+
+while (index < myArray.length) {
+    console.log(myArray[index]);
+    index++;
+};
+
+// Utilizando el ciclo do while
+index = 0;
+
+do {
+    console.log(myArray[index]);
+    index++;
+} while (index < myArray.length);
+
