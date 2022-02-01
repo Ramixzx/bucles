fb64601eb51606032c0bc247b23271fbfd6516be
Ramixzx <ramiro.mijalovich@gmail.com>
2022-02-01 10:31:52 -0300

    Se inicializo npm. Se finalizo con el ejercicio1 utilizando los ciclos for y while

---
+++ src/ejercicio1/index.js
@@ -0,0 +1,11 @@
+let vuelta = 1;
+let limite = 17;
+
+while (vuelta <= limite) {
+    console.log(`Vuelta numero ${vuelta}`);
+    vuelta++;
+};
+
+for(vuelta; vuelta <= limite; vuelta++){
+    console.log(`Vueltita numero ${vuelta}`);
+};
