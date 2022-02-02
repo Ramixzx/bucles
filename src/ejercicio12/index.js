bdc62c8ab9b3d3405116be2e2e0699022f3b58d9
Ramixzx <ramiro.mijalovich@gmail.com>
2022-02-01 22:04:58 -0300

    The exercise is finished.In addition to complying with what was requested, a function was created that automates the division of array elements.

---
+++ src/ejercicio12/index.js
@@ -0,0 +1,23 @@
+let myArray = [3344,34334,454543,342534,4563456,3445,23455,234,262,2335,43323,4356,345,4545,452,345,434,36,345,4334,5454,345,4352,23,365,345,47,63,425,6578759,768,834,754,35,32,445,453456,56,7536867,3884526,4234,35353245,53244523,566785,7547,743,4324,523472634,26665,63432,54645,32,453625,7568,5669576,754,64356,542644,35,243,371,3251,351223,13231243,734,856,56,53,234342,56,545343];
+
+myArray.forEach(function(item){
+
+    // If the difference (module) between "item" and "14" is 0 then execute
+    if(item % 14 === 0) {
+        console.log(`The number ${item} is divisible by 14`);
+    } 
+});
+
+
+// This function has an array and a divider as parameters.
+const isDivisible = (array, divider) => {
+    array.forEach(function(item){
+        // If the difference (module) between "item" and "divider" is 0 then execute
+        if(item % divider === 0) {
+            console.log(`The number ${item} is divisible by ${divider}`);
+        }  
+    });
+}
+
+// Example
+isDivisible(myArray, 23);
